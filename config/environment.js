707e390f94078a88801c5e04ff2a8e46bdeaa8e6
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-03-12 17:24:07 +1100

    Add titles for first pages

--- config/environment.js
+++ config/environment.js
@@ -5,40 +5,30 @@ module.exports = function(environment) {
     modulePrefix: 'lmpa-interflux-com',
     environment: environment,
     rootURL: '/',
-    locationType: 'auto',
+    locationType: 'history',
+
+    autoprefixer: {
+      browsers: ['> 1% in AU', 'Explorer > 9', 'Firefox >= 17', 'Chrome >= 10', 'Safari >= 6', 'iOS >= 6']
+    },
+
     EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      },
+      FEATURES: {},
       EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
         Date: false
       }
     },
 
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
-    }
+    APP: {}
   };
 
   if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+
   }
 
   if (environment === 'test') {
-    // Testem prefers this...
     ENV.locationType = 'none';
-
-    // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
-
     ENV.APP.rootElement = '#ember-testing';
   }
 
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,24 +1,24 @@
-/*jshint node:true*/
+/* jshint node:true */
 /* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const env = EmberApp.env();
+const isProductionLikeBuild = ['production', 'staging'].includes(env);
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    // Add options here
-  });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  const app = new EmberApp(defaults, {
+    // Makes SASS listent to file changes in the component folders
+    sassOptions: {
+      includePaths: ['app/components']
+    },
+    // Adds CSS browser prefixes
+    autoprefixer: {
+      browsers: ['> 1%', 'Explorer > 9', 'Firefox >= 17', 'Chrome >= 10', 'Safari >= 6', 'iOS >= 6'],
+      cascade: false,
+      remove: false
+    }
+  });
 
   return app.toTree();
 };
