c9d40c3a76a646445429c461bd3ca12114920fce
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-04-13 02:08:00 +1000

    Add icons, check svg, new styles for page wrapping UI

---
+++ app/components/page-nav/component.js
@@ -0,0 +1,41 @@
+import Ember from 'ember';
+
+const sequence = [
+  'locale.intro',
+  'locale.speed',
+  'locale.voiding',
+  'locale.costs',
+  'locale.heat-failures',
+  'locale.dross',
+  'locale.wetting',
+  'locale.interflux',
+  'locale.contact'
+];
+const lastIndex = sequence.length - 1;
+
+export default Ember.Component.extend({
+  tagName: 'nav',
+  router: Ember.inject.service('-routing'),
+
+  allRoutes: sequence,
+
+  // Returns position of current route in the sequence
+  currentIndex: Ember.computed('router.currentRouteName', function() {
+    return sequence.indexOf(this.get('router.currentRouteName'));
+  }),
+
+  // Returns next route in sequence
+  nextRoute: Ember.computed('router.currentRouteName', function() {
+    let next = this.get('currentIndex') + 1;
+    next = next > lastIndex ? 0 : next;
+    return sequence[next];
+  }),
+
+  // Returns previous route in sequence
+  prevRoute: Ember.computed('router.currentRouteName', function() {
+    const currentIndex = sequence.indexOf(this.get('router.currentRouteName'));
+    let prev = this.get('currentIndex') - 1;
+    prev = prev < 0 ? lastIndex : prev;
+    return sequence[prev];
+  })
+});
--- app/components/svg-check/component.js
+++ app/components/svg-check/component.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  elementId: 'page-wrapper'
+  classNames: ['check']
 });
