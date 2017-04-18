e597457e9b8fe39ffa86b91b396d1f2147ad6b67
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-04-18 20:51:21 +1000

    Content updates

--- app/components/interflux-page/component.js
+++ app/components/interflux-page/component.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
+const { computed } = Ember;
+
 export default Ember.Component.extend({
   tagName: 'main',
-  elementId: 'interflux'
+  elementId: 'interflux',
+  years: computed(function() {
+    const currentYear = new Date().getFullYear();
+    const foundationYear = 1980;
+    return currentYear - foundationYear;
+  })
 });
