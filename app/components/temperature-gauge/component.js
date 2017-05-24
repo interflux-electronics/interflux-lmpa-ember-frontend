af3898950b5a65d6d3d412e54dba35dff8e588eb
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-05-24 22:03:47 +1000

    Improve animation

--- app/components/temperature-gauge/component.js
+++ app/components/temperature-gauge/component.js
@@ -17,67 +17,62 @@ const limitsMap = {
   }
 };
 
+let timer = {
+  lmpa: null,
+  sac: null
+};
+
 export default Ember.Component.extend({
-  classNames: ['temperature-guage'],
+  elementId: 'temperature-guage',
 
   observeProcess: observer('process', function() {
-    this.animateArrow('lmpa');
+    // run.cancel(timer.lmpa);
+    // run.cancel(timer.sac);
+    // this.animateArrow('lmpa');
     // this.animateArrow('sac');
   }),
 
-  randomBM() {
+  // Return random number between 0 and 1 with a normal distribution
+  random() {
     return (1 + ((Math.random() + Math.random() + Math.random() + Math.random() + Math.random() + Math.random()) - 3) / 3) / 2;
   },
 
-  // animateArrow(type) {
-  //   const process = this.get('process');
-  //   const limits = limitsMap[process][type];
-  //   const min = limits[0];
-  //   const max = limits[1];
-  //   const range = max - min;
-  //   const random = this.randomBM();
-  //   const temperature = min + (random * range);
-  //   const degrees = (temperature * ((2 * 108) / 350)) - 108;
-  //   const $lmpa = this.$(`#arrow-${type}>g`);
-  //   $lmpa.stop('velocity').velocity({
-  //     rotateZ: `${degrees}deg`
-  //   }, {
-  //     duration: 2000,
-  //     easing: 'easeOutExpo'
-  //   });
-  //   const self = this;
-  //   run.later(self, function() {
-  //     self.animateArrow(type);
-  //   }, 2100);
-  // },
-
-  animateArrow(type) {
+  animateArrow(alloy, intro) {
+    const self = this;
     const process = this.get('process');
-    const limits = limitsMap[process][type];
+    const limits = limitsMap[process][alloy];
     const min = limits[0];
     const max = limits[1];
     const range = max - min;
-    const random = this.randomBM();
-    const temperature = min + (range / 4) + (random * (range / 2));
+    const random = this.random();
+    // const temperature = min + (range / 4) + (random * (range / 2));
+    const temperature = min + (range * 0.166) + (random * (range * 0.666));
     const degrees = (temperature * ((2 * 108) / 350)) - 108;
-    const duration = 1000 + (random * 2000);
-    const $lmpa = this.$(`#arrow-${type}>g`);
-    console.log(`${type} ${degrees}`);
-    $lmpa.stop('velocity').velocity({
-      rotateZ: `${degrees}deg`
-    }, {
-      duration: duration,
-      easing: 'easeInOut'
-    });
-    const self = this;
-    run.later(self, function() {
-      self.animateArrow(type);
-    }, duration * 1.1);
+    const duration = 1000 + (random * 1400);
+    const $lmpa = this.$(`#arrow-${alloy}>g`);
+    if (intro) {
+      $lmpa.stop('velocity').velocity({
+        rotateZ: [`${degrees}deg`, -108]
+      }, {
+        duration: 7000,
+        easing: 'easeInOut'
+      });
+    } else {
+      $lmpa.stop('velocity').velocity({
+        rotateZ: `${degrees}deg`
+      }, {
+        duration: duration,
+        easing: 'easeInOut'
+      });
+    }
+    const delay = intro ? 7200 : duration * 1.1;
+    timer[alloy] = run.later(self, function() {
+      self.animateArrow(alloy);
+    }, delay);
   },
 
   didInsertElement: function() {
-    // this.moveArrows();
-    // this.animateArrow('lmpa', 1);
-    // this.animateArrow('sac', 1);
+    this.animateArrow('lmpa', true);
+    this.animateArrow('sac', true);
   }
 });
