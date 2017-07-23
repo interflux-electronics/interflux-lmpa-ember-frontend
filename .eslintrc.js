650af499270e136cac7828483849ef48062c8c5d
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-07-23 15:22:00 +1000

    Edit ES Lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,13 @@ module.exports = {
     browser: true
   },
   rules: {
+    'no-debugger': 1,
+    'no-console': 1,
+    'no-unused-vars': 1,
+    'indent': [2, 2],
+    'semi': ['error', 'always']
+  },
+  globals: {
+    ga: true
   }
 };
