effb762ea7ac2d0ff8ff3563d7d453298dfd4adc
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-10-18 21:31:03 +1100

    Spelling update

--- app/routes/locale/interflux.js
+++ app/routes/locale/interflux.js
@@ -2,5 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   seoTitle: `LMPA was developed with 37 years of experience - Interflux`,
-  seoDescription: `Interflux® Electronics NV has been has researching and developing soldering chemistry since 1980. That's 37 years of know-how bundled into the LMPA™.`,
+  seoDescription: `Interflux® Electronics NV has been researching and developing soldering chemistry since 1980. That's 37 years of know-how bundled into the LMPA™.`,
 });
