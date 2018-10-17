3bfcd98ede4c320313266df8e5f6d262162f7f6b
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-10-18 10:38:35 +1100

    Upgrade ES Lint configs, SASS and Autoprefixer

--- app/router.js
+++ app/router.js
@@ -1,25 +1,25 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 import { inject as service } from '@ember/service';
-import { getOwner } from '@ember/application';
+// import { getOwner } from '@ember/application';
 import { on } from '@ember/object/evented';
 
 const Router = EmberRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL,
-  seo: service(),
-  googleAnalytics: service(),
+  // seo: service(),
+  // googleAnalytics: service(),
 
   onInit: on('init', function() {
-    this.googleAnalytics.startTracking();
+    // this.googleAnalytics.startTracking();
   }),
 
   onEachDidTransition: on('didTransition', function() {
-    const currentRoute = getOwner(this).lookup(
-      'route:' + this.currentRouteName
-    );
-    this.seo.setMetaTags(currentRoute);
-    this.googleAnalytics.sendPageView(currentRoute);
+    // const currentRoute = getOwner(this).lookup(
+    //   'route:' + this.currentRouteName
+    // );
+    // this.seo.setMetaTags(currentRoute);
+    // this.googleAnalytics.sendPageView(currentRoute);
   })
 });
 
--- app/services/google-analytics.js
+++ app/services/google-analytics.js
@@ -1,35 +1,35 @@
-import Service from '@ember/service';
-import config from '../config/environment';
-
-const ready =
-  config.googleAnalytics && config.googleAnalytics.trackingId && ga
-    ? true
-    : false;
-
-export default Service.extend({
-  startTracking() {
-    if (!ready) {
-      return;
-    }
-    ga('create', {
-      trackingId: config.googleAnalytics.trackingId
-    });
-    ga('set', {
-      dimension1: config.modulePrefix,
-      dimension2: config.environment
-    });
-  },
-
-  sendPageView(currentRoute) {
-    if (!ready) {
-      return;
-    }
-    ga('set', {
-      page: window.location.pathname,
-      hostname: window.location.host,
-      title: document.title,
-      dimension3: currentRoute.routeName.replace(/\./g, '/')
-    });
-    ga('send', 'pageview');
-  }
-});
+// import Service from '@ember/service';
+// import config from '../config/environment';
+//
+// const ready =
+//   config.googleAnalytics && config.googleAnalytics.trackingId && ga
+//     ? true
+//     : false;
+//
+// export default Service.extend({
+//   startTracking() {
+//     if (!ready) {
+//       return;
+//     }
+//     ga('create', {
+//       trackingId: config.googleAnalytics.trackingId
+//     });
+//     ga('set', {
+//       dimension1: config.modulePrefix,
+//       dimension2: config.environment
+//     });
+//   },
+//
+//   sendPageView(currentRoute) {
+//     if (!ready) {
+//       return;
+//     }
+//     ga('set', {
+//       page: window.location.pathname,
+//       hostname: window.location.host,
+//       title: document.title,
+//       dimension3: currentRoute.routeName.replace(/\./g, '/')
+//     });
+//     ga('send', 'pageview');
+//   }
+// });
