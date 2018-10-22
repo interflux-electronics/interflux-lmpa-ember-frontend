b24a82d07443eff70c0c76db52349015b955df6c
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-10-22 14:51:00 +1100

    Update the deploy script to include Yarn and Node

--- fastboot-server.js
+++ fastboot-server.js
@@ -5,7 +5,8 @@ const FastBootAppServer = require('fastboot-app-server');
 let server = new FastBootAppServer({
   distPath: 'dist',
   gzip: true,
-  host: '127.0.0.1'
+  host: '127.0.0.1',
+  port: 8000
 });
 
 server.start();
