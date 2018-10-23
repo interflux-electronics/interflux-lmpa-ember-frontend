00fe115107d5e2850e597ff59f353b0a9d224609
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-10-24 09:23:13 +1100

    Add Fastboot watch notifier

--- fastboot-server.js
+++ fastboot-server.js
@@ -1,9 +1,21 @@
 /*eslint-env node*/
 
 const FastBootAppServer = require('fastboot-app-server');
+const FastBootWatchNotifier = require('fastboot-watch-notifier');
 
-let server = new FastBootAppServer({
-  distPath: 'dist',
+const distPath = 'current/dist';
+
+const notifier = new FastBootWatchNotifier({
+  distPath: 'current/dist',
+  debounceDelay: 250,
+  saneOptions: {
+    poll: true
+  }
+});
+
+const server = new FastBootAppServer({
+  distPath,
+  notifier,
   gzip: true,
   host: '127.0.0.1',
   port: 8000
