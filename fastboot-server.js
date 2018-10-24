3730bf9f451cba5de965ec131d738b0193809b9d
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-10-24 21:19:21 +1100

    Improve the deploy script

--- fastboot-server.js
+++ fastboot-server.js
@@ -3,10 +3,10 @@
 const FastBootAppServer = require('fastboot-app-server');
 const FastBootWatchNotifier = require('fastboot-watch-notifier');
 
-const distPath = 'current/dist';
+const distPath = 'dist';
 
 const notifier = new FastBootWatchNotifier({
-  distPath: 'current/dist',
+  distPath,
   debounceDelay: 250,
   saneOptions: {
     poll: true
@@ -17,7 +17,7 @@ const server = new FastBootAppServer({
   distPath,
   notifier,
   gzip: true,
-  host: '127.0.0.1',
+  host: '0.0.0.0',
   port: 8000
 });
 
