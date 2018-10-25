558fdf8deaf7ca360bfc720a8ee1097f1919604e
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-10-26 10:11:10 +1100

    Update git deploy hooks with rm and cp current

--- fastboot-server.js
+++ fastboot-server.js
@@ -3,7 +3,7 @@
 const FastBootAppServer = require('fastboot-app-server');
 const FastBootWatchNotifier = require('fastboot-watch-notifier');
 
-const distPath = 'dist';
+const distPath = '/var/www/lmpa.interflux.com/current';
 
 const notifier = new FastBootWatchNotifier({
   distPath,
