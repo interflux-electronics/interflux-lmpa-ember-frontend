c8889bf973fbb8266ae41c7ba12a61a6e41883a4
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-10-25 12:19:21 +1100

    Add shell script to build Fastboot

---
+++ build-production.sh
@@ -0,0 +1,28 @@
+#!/usr/local/bin/fish
+
+# /bin/bash
+# set -e
+# set -o pipefail
+
+echo "----------"
+echo "GIT POST RECEIVE HOOK"
+echo "User: $USER"
+echo "Host: $HOSTNAME"
+echo "Path: $PWD"
+
+echo "\$ git fetch"
+git fetch
+
+echo "\$ git checkout feature/deploy-pipeline -f"
+git checkout feature/deploy-pipeline -f
+
+# echo "\$ nvm install"
+# nvm install
+
+echo "\$ yarn install"
+yarn install
+
+echo "\$ ember build -prod"
+ember build -prod
+
+echo "----------"
