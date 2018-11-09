f304b0ff7d8a3a68ce7c1ced2f2926b1db5886d2
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-11-09 14:19:16 +1100

    Retrofit button-link component with href-to helper

--- app/components/button-link/component.js
+++ app/components/button-link/component.js
@@ -1,6 +1,19 @@
-import LinkComponent from '@ember/routing/link-component';
+import ButtonBase from '../button-base/component';
+import { hrefTo } from 'ember-href-to/helpers/href-to';
+import { computed } from '@ember/object';
 
-export default LinkComponent.extend({
-  classNames: ['button-link button-base'],
-  classNameBindings: ['type']
+export default ButtonBase.extend({
+  tagName: 'a',
+  classNames: ['button-link'],
+  attributeBindings: ['href'],
+
+  // Passed in
+  route: undefined,
+  text: undefined,
+  icon: undefined,
+
+  // Return the URL matching the route
+  href: computed('route', function() {
+    return hrefTo(this, this.route);
+  })
 });
