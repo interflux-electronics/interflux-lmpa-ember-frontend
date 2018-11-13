7fa79c838021cf2f0eb703f8e7b57087e3bd1a1a
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-11-13 22:26:51 +1100

    Remove comments form application serializer

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,12 +1,3 @@
-// import { dasherize } from '@ember/string';
-
 import JSONAPISerializer from 'ember-data/serializers/json-api';
 
-export default JSONAPISerializer.extend({
-  // keyForAttribute(attr) {
-  //   return dasherize(attr);
-  // },
-  // keyForRelationship(attr) {
-  //   return dasherize(attr);
-  // }
-});
+export default JSONAPISerializer.extend({});
