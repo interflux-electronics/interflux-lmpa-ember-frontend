3ed4932be4888cb695cfbab70e860f7fdc756fcd
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-11-19 15:58:05 +1100

    Autoset interflux-human flag

--- app/services/user.js
+++ app/services/user.js
@@ -2,6 +2,24 @@ import Service from '@ember/service';
 import { computed } from '@ember/object';
 
 export default Service.extend({
+  // By adding #interflux-human to the URL the user will now be
+  // remembered as an Interflux member and excluded from analytics.
+  init() {
+    this._super(...arguments);
+    if (window.location.hash === '#interflux-human') {
+      localStorage.setItem('interflux-human', true);
+      this.removeHashFromURL();
+    }
+  },
+
+  removeHashFromURL() {
+    history.pushState(
+      '',
+      document.title,
+      window.location.pathname + window.location.search
+    );
+  },
+
   // To avoid Interfluxers from messing with the conversion stats we ask them
   // to set a boolean in their localStorage.
   // localStorage.setItem('interflux-human', true)
