c239162513a337beaa7762ae0a9ba6158303d6c4
Jan Werkhoven <jan.werkhoven@gmail.com>
2023-11-26 17:02:19 +1100

    Programatically play the videos because Chrome blocks autoplay

--- app/components/page-speed/component.js
+++ app/components/page-speed/component.js
@@ -1,5 +1,18 @@
 import PageComponent from '../page-base/component';
 
 export default PageComponent.extend({
-  elementId: 'page-speed'
+  elementId: 'page-speed',
+
+  didInsertElement() {
+    this._super(...arguments);
+    document.querySelectorAll('video').forEach(video => {
+      video.play();
+    });
+  },
+
+  click() {
+    document.querySelectorAll('video').forEach(video => {
+      video.play();
+    });
+  }
 });
